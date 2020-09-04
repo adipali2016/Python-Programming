dc3734ce3d88aea91bc32fe29251fbfd537432a0
Ayush Singh <19u01045@iiitbhopal.ac.in>
2020-09-04 20:32:41 +0530

    Strings and Typecasting :)

---
+++ string_operations.py
@@ -0,0 +1,21 @@
+sasti_string = "Chacha Vidhayak hain Humare"
+
+print(sasti_string[0:7])
+print(len(sasti_string)) #length of string
+#print(sasti_string[120]) this will throw error
+
+#this will give full string
+print(sasti_string[0:120])
+print()
+print(sasti_string[0:5:2])#advance slicing
+print(sasti_string[::-1])#It will reverse the srting
+'''
+sasti_string.isalpha()
+sasti_string.isalnum()
+sasti_string.endswith()
+sasti_string.count("")
+sasti_string.capitalize()
+sasti_string.find()
+sasti_string.lower()
+sasti_string.replace("","")
+'''
---
+++ variables_typecasting.py
@@ -0,0 +1,26 @@
+'''
+var1 = 'Hello World'
+var2 = 'Ahoy World'
+var3 = 36.55
+print(var1 + var2)
+
+print(2* "Hello World\n")
+
+var5 = "44"
+var6 = "112"
+
+print(100 *str(int(var5) + int(var6))) #typecasting
+
+print("ENter the number here...")
+apnainput = input()
+
+print("Value odf apna input is .... ", int(apnainput)+2)
+'''
+
+# Building a calculator
+
+print("Enetr the number 1")
+num1 = input()
+print("ENter the number 2")
+num2 = input()
+print(int(num1)+int(num2))

3fedd629c1c85e649842efc240b848c34553feca
Aditya Paliwal <adipali2016@gmail.com>
2020-10-01 23:29:15 +0530

    Another way of writing main function

--- main1.py
+++ main1.py
@@ -1,1 +1,10 @@
 print("Hello World 7")
+
+# Another way of writing a main function is 
+
+if __name__ = '__main__':
+  print("Hello World")
+  
+'''
+In this way you will know if this python file is called as main or not. If this file is not called as print("Hello World") will not run
+'''
